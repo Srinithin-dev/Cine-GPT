6bb4a48989d3b9fdc29e44d7f3e6bd6d31f20726
srinithin <tsrinithin@gmail.com>
2026-07-02 20:20:16 +0530

    initial setup with vite tailwind postcss autofixer

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  console.log("lkj");
+  return <div className="text-xl text-red-600">Hi there</div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const rootElement = createRoot(document.getElementById("root")!);
+rootElement.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
